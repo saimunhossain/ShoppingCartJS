a16b445d7c443ddbb4df774605ae3b8f9a8438d5
Md Saimun Hossain <saimonanam@gmail.com>
2019-03-23 01:50:10 +0600

    selected products have been appeare to cart items

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 'use strict';
 
+const cartDOM = document.querySelector('.cart');
 const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
-addToCartButtonsDOM.forEach(addToCartButonDOM => {
-    addToCartButonDOM.addEventListener('click', () => {
-        const productDOM = addToCartButonDOM.parentNode;
+
+addToCartButtonsDOM.forEach(addToCartButtonDOM => {
+    addToCartButtonDOM.addEventListener('click', () => {
+        const productDOM = addToCartButtonDOM.parentNode;
         const product = {
             image: productDOM.querySelector('.product__image').getAttribute('src'),
             name: productDOM.querySelector('.product__name').innerText,
             price: productDOM.querySelector('.product__price').innerText,
         };
-        console.table(product);
+        cartDOM.insertAdjacentHTML('beforeend', `
+        <div class="cart__item">
+            <img class="cart__item__image" src="${product.image}" alt="${product.name}">
+            <h3 class="cart__item__name">${product.name}</h3>
+            <h3 class="cart__item__price">${product.price}</h3>
+        </div>
+        `);
     });
 });
