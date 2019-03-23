bca653cda9fb9e5cdaa1f6409e5f84c05cd1520c
Md Saimun Hossain <saimonanam@gmail.com>
2019-03-23 15:54:42 +0600

    cart items increase/decrease/remove button added

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
             image: productDOM.querySelector('.product__image').getAttribute('src'),
             name: productDOM.querySelector('.product__name').innerText,
             price: productDOM.querySelector('.product__price').innerText,
+            quantity: 1,
         };
 
         const isInCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0);
@@ -21,6 +22,10 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
                 <img class="cart__item__image" src="${product.image}" alt="${product.name}">
                 <h3 class="cart__item__name">${product.name}</h3>
                 <h3 class="cart__item__price">${product.price}</h3>
+                <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
+                <h3 class="cart__item__quantity">${product.quantity}</h3>
+                <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+                <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
             </div>
             `);
             cart.push(product);
