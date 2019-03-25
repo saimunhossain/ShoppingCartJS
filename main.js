73613c6650b8885cdb1737cb64ba00ecf6596a17
Md Saimun Hossain <saimonanam@gmail.com>
2019-03-25 23:22:35 +0600

    cart items increamenent/decreament buttons working fine

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ if(cart.length > 0){
                 <img class="cart__item__image" src="${product.image}" alt="${product.name}">
                 <h3 class="cart__item__name">${product.name}</h3>
                 <h3 class="cart__item__price">${product.price}</h3>
-                <button class="btn btn--primary btn--small btn--danger" data-action="DECREASE_ITEM">&minus;</button>
+                <button class="btn btn--primary btn--small${(product.quantity === 1 ? ' btn--danger' : '')}" data-action="DECREASE_ITEM">&minus;</button>
                 <h3 class="cart__item__quantity">${product.quantity}</h3>
                 <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
                 <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
@@ -102,7 +102,7 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
                 <img class="cart__item__image" src="${product.image}" alt="${product.name}">
                 <h3 class="cart__item__name">${product.name}</h3>
                 <h3 class="cart__item__price">${product.price}</h3>
-                <button class="btn btn--primary btn--small btn--danger" data-action="DECREASE_ITEM">&minus;</button>
+                <button class="btn btn--primary btn--small btn--danger${(product.quantity === 1 ? ' btn--danger' : '')}" data-action="DECREASE_ITEM">&minus;</button>
                 <h3 class="cart__item__quantity">${product.quantity}</h3>
                 <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
                 <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
